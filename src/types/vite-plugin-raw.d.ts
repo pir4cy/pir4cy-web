f562f332414343b400e583522cd3def72ce3c533
pir4cy <kt145@hotmail.com>
2025-04-26 16:48:11 -0400

    another update to deploy blogs via netlify

---
+++ src/types/vite-plugin-raw.d.ts
@@ -0,0 +1,9 @@
+declare module 'vite-plugin-raw' {
+  interface RawPluginOptions {
+    match: RegExp;
+    exclude?: RegExp[];
+  }
+  
+  function raw(options: RawPluginOptions): any;
+  export default raw;
+} 
